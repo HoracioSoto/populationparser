954aebc09e4c94488ac8b007b7506c9dd2fc0cd1
Hito <Hito@Hito-HP>
2014-11-05 23:07:50 -0300

    fileToArray functions work

--- PopulationApp/fileToArray2.php
+++ PopulationApp/fileToArray2.php
@@ -1,32 +1,33 @@
 <?php
-/**
-File format example:
-Province,Capital
-    City1,PostalCode,Population
-    City2,PostalCode,Population
-    City3,PostalCode,Population
-Chaco,Resistencia
-    Barranqueras,3500,32000
-    Resistencia,3500,1000000
-    Saenz Peña,3500,75000
- * */
-/**
-array(  array( 'name'    => 'ProvinceName1',
-               'capital' => 'CapitalName1',
-               'cities'  => array(
-                                    array( 'name' => 'Cityname', 'postalcode' => 100, 'population' => 100),
-                                    array( 'name' => 'Cityname2', 'postalcode' => 100, 'population' => 100)
-                            )
-        ),
-        array( 'name'    => 'Corrientes',
-               'capital' => 'Corrientes',
-               'cities'  => array(
-                                    array( 'name' => 'Corrientes', 'postalcode' => 3400, 'population' => 100000000),
-                                    array( 'name' => 'Mercedes', 'postalcode' => 3450, 'population' => 34565)
-                            )
-        )
-)
-**/
+/*
+ * File format example:
+ * Province,Capital
+ *     City1,PostalCode,Population
+ *     City2,PostalCode,Population
+ *     City3,PostalCode,Population
+ * Chaco,Resistencia
+ *     Barranqueras,3500,32000
+ *     Resistencia,3500,1000000
+ *     Saenz Peña,3500,75000
+ */
+
+/*
+ * array(  array( 'name'    => 'ProvinceName1',
+ *                'capital' => 'CapitalName1',
+ *                'cities'  => array(
+ *                                     array( 'name' => 'Cityname', 'postalcode' => 100, 'population' => 100),
+ *                                     array( 'name' => 'Cityname2', 'postalcode' => 100, 'population' => 100)
+ *                             )
+ *         ),
+ *         array( 'name'    => 'Corrientes',
+ *                'capital' => 'Corrientes',
+ *                'cities'  => array(
+ *                                     array( 'name' => 'Corrientes', 'postalcode' => 3400, 'population' => 100000000),
+ *                                     array( 'name' => 'Mercedes', 'postalcode' => 3450, 'population' => 34565)
+ *                             )
+ *         )
+ * )
+ */
 
 /**
  * File to array
